eab786042cda8722d70bd9624e8245b3f2ab6b39
hideaki <hs.wgp.46@gmail.com>
2022-10-06 20:49:18 +0900

    product.Module追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './common/navbar/navbar.component';
-import { ProductListingsComponent } from './product-listings/product-listings.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    ProductListingsComponent,
+
   ],
   imports: [
     BrowserModule,
